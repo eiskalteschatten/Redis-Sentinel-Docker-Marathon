70db6fbf48381293279fe30a9e784c46537d5289
Alex Seifert <seifert@jochen-schweizer.de>
2016-11-16 16:50:35 +0100

    Removed unncessary lines

--- redis-slave/redis-entrypoint.sh
+++ redis-slave/redis-entrypoint.sh
@@ -3,11 +3,6 @@
 MASTER_IP=`getent hosts ${MASTER_HOST} | awk '{ print $1 }'`
 MYIP=`getent hosts ${HOST} | awk '{ print $1 }'`
 
-echo "---------------------------------------------"
-echo "Redis Master: ${MASTER_IP}:${MASTER_PORT}"
-echo "My IP Address and Port: ${MYIP}:${PORT0}"
-echo "---------------------------------------------"
-
 sed -i "s/\$MYIP/$MYIP/g" /redis/redis.conf
 sed -i "s/\$PORT0/$PORT0/g" /redis/redis.conf
 
